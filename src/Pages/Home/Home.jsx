6bc10fc0c99ac51b67aaba7e93565d1b8332cee3
Busra2456 <you@example.com>
2024-10-31 18:22:59 +0600

    Dragon News Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,24 +3,55 @@ import Header from "../Shared/Navbar/Header/Header";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSite from "../Shared/Rightsite/RightSite";
 import BreakingNews from "./BreakingNews";
-
+import user from "../../assets/assets/user.png"
+import { FaShareAlt } from "react-icons/fa";
 
 const Home = () => {
       return (
             <div>
+                 
+
+
                   <Header></Header>
                   <BreakingNews></BreakingNews>
                   <Navbar ></Navbar>
                   
                   <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-                  <div className="border" >
+                  <div className="" >
                  
                         <LifesideNav></LifesideNav>
                   </div>
-                  <div className="md:col-span-2 border">
-                        <h2 className="text-2xl">News Coming soon</h2>
-                  </div>
-                  <div >
+                 
+                 
+                 {/* LifesideNav------------------------------------------------------------ */}
+                 
+                    
+                
+              <div className="md:col-span-2 ">
+                      
+              <h2 className="text-3xl font-bold">Dragon News Home</h2>      
+                       
+                 
+               
+                <div className="flex mt-10 bg-slate-200 px-4 py-4 border border-fuchsia-600" >
+                  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar ">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src={user} />
+
+      </div>
+            
+              </div>
+
+              </div>
+              </div>
+
+{/* RightSite------------------------------------------------------------------------------------ */}
+
+
+
+<div >
                         <RightSite></RightSite>
                   </div>
                   </div>
--- src/Pages/Shared/LeftSide/LifesideNav.jsx
+++ src/Pages/Shared/LeftSide/LifesideNav.jsx
@@ -1,9 +1,87 @@
+import picture1 from "../../../assets/assets/1.png"
+import picture2 from "../../../assets/assets/2.png"
+import picture3 from "../../../assets/assets/3.png"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LifesideNav = () => {
+      const [allCategory,setAllCategory ] = useState([]);
+      useEffect(() =>{
+            fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setAllCategory(data))
+      },[])
       return (
-            <div>
-               <h2 className="text-3xl">Life Side</h2>   
+     
+            <div className="space-y-6" >
+              <h1 className="text-3xl font-bold mt-8 ">All Category  </h1> 
+              {
+                allCategory.map(category => <Link 
+                  className="block ml-4 text-xl font-bold"
+                  key={category.id} to={`/category/${category.id} `}  >
+                 {category.name} </Link>)
+                 
+              }
+
+              {/* picture-1 */}
+
+<div>
+
+                    
+<div className="card card-compact bg-base-100 rounded mx-4">
+  <figure>
+    <img
+      src={picture1}
+      alt=""/>
+  </figure>
+  <div className="card-body">
+   
+    <p className="text-xl" >Bayern Slams Authorities Over Flight Delay to Club World Cup </p>
+    <p>Sports  Jan 4, 2022</p>
+   
+  </div>
+</div>
+        
+
+        {/* picture-2 */}
+
+                      
+        <div className="card card-compact bg-base-100 rounded mx-4">
+  <figure>
+    <img
+      src={picture2}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+   
+    <p className=" text-xl" >Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+    <p>Sports  Jan 4, 2022</p>
+  </div>
+</div>
+        {/*picture-3*/}
+
+                      
+        <div className="card card-compact bg-base-100 rounded mx-4">
+  <figure>
+    <img
+      src={picture3}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+   
+    <p className=" text-xl" >Bayern Slams Authorities Over Flight Delay to Club World Cup </p>
+    <p>Sports  Jan 4, 2022</p>
+  </div>
+</div>
+        
+      
+      
+      </div>     
+
+
+
+              
             </div>
       );
 };
