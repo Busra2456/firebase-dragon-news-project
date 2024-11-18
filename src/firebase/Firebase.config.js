8a64786538aabfc084a3d14a339cac64975e7738
Busra2456 <you@example.com>
2024-11-18 11:50:20 +0600

    firebaseConfig

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -9,13 +9,12 @@ console.log('inside firebase config',import.meta.env.VITE_PASS)
 
 const firebaseConfig = {
 
-
-apiKey:import.meta.env.VITE_AUTHDOMAIN,
-authDomain:import.meta.env.VITE_PROJECTID,
-projectId:import.meta.env.VITE_STORAGEBUCKET,
-storageBucket:import.meta.env.VITE_APIKEY,
-messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
-appId:import.meta.env.VITE_APPID
+apiKey:import.meta.env.VITE_APIKEY ,
+authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+projectId:import.meta.env.VITE_PROJECTID ,
+storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+appId:import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
@@ -23,9 +22,4 @@ const app = initializeApp(firebaseConfig);
 export default app;
 
 
-// apiKey: "AIzaSyDAcbh48zDBD-m-mEJN_WQFDNloGTle-co",
-// authDomain: "fir-dragon-news-project.firebaseapp.com",
-// projectId: "fir-dragon-news-project",
-// storageBucket: "fir-dragon-news-project.firebasestorage.app",
-// messagingSenderId: "602445877185",
-// appId: "1:602445877185:web:88257cb6f9723360bb6940"
+
